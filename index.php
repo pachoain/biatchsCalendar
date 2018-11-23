71bef55cc15120ec7506ef65854d05fee6f7d2dc
pachoain <pierre-alexandre.choain@isen.yncrea.fr>
2018-11-23 21:29:43 +0100

    Création des fichiers index.php et style.css

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8" />
+        <link rel="stylesheet" type="text/css" href="style.css"/>
+        <title>Calendrier de l'Avent des Biatchs</title>
+    </head>
+
+    <body>
+      
+    </body>
+</html>
